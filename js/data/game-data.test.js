7f54188d9e15007afdd7a2ee9cb138189bc4668a
Denis <derevyankindenis@mail.ru>
2017-10-13 12:56:04 +0300

    Удалил, того, что нет в задании по настройке окружения для тестов

--- js/data/game-data.test.js
+++ js/data/game-data.test.js
@@ -1,50 +1,9 @@
 import assert from 'assert';
-import {calculateScore, Timer} from '../utils';
 
-describe(`Тестирование функции подсчета очков`, () => {
-  it(`Если игрок ответил меньше чем на 10 ответов, то очки не начисляются`, () => {
-    assert.equal(calculateScore([], 0), -1);
-    assert.equal(calculateScore([0, 0], 1), -1);
-    assert.notEqual(calculateScore([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 1), -1);
+describe(`Array`, () => {
+  describe(`#indexOf()`, () => {
+    it(`should return -1 when the value is not present`, () => {
+      assert.equal(-1, [1, 2, 3].indexOf(4));
+    });
   });
-
-  it(`Если у игрока кончились жизни, то очки не начисляются`, () => {
-    assert.equal(calculateScore([1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 0), -1);
-  });
-
-  it(`Если у игрока остались все жизни и он ответил на все вопросы не быстро и не медленно, то начисляется 1150 очков`, () => {
-    assert.equal(calculateScore([1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 3), 1150);
-  });
-
-  it(`Если игрок ответил на все вопросы быстро и сохранил все жизни, то ему наисляется 1650`, () => {
-    assert.equal(calculateScore([3, 3, 3, 3, 3, 3, 3, 3, 3, 3], 3), 1650);
-  });
-
-  // ...
-
-});
-
-
-
-
-
-describe(`Тестирование функции создания таймера`, () => {
-
-  it(`Если задано отрицательное значение таймера, он инициализируется нулём`, () => {
-    assert.equal((new Timer(-10)).tick(), 0);
-  });
-
-  it(`Если время вышло, tick() возвращает 0`, () => {
-    assert.equal((new Timer(1)).tick(), 0);
-  });
-
-  it(`Каждый запуск tick() уменьшает таймер на 1`, () => {
-    const timer = new Timer(10);
-    assert.equal(timer.tick(), 9);
-    assert.equal(timer.tick(), 8);
-    assert.equal(timer.tick(), 7);
-  });
-
-  // ...
-
 });
