686243d576aed5b05ed66655a5d98ac69871b758
Denis <derevyankindenis@mail.ru>
2017-10-15 10:13:30 +0300

    Написал тесты для функций таймера и подсчета очков

--- js/data/game-data.test.js
+++ js/data/game-data.test.js
@@ -1,9 +1,49 @@
 import assert from 'assert';
+import {calculateScore, Timer} from './game-data';
 
-describe(`Array`, () => {
-  describe(`#indexOf()`, () => {
-    it(`should return -1 when the value is not present`, () => {
-      assert.equal(-1, [1, 2, 3].indexOf(4));
-    });
+describe(`Тестирование функции подсчета очков`, () => {
+  it(`Если игрок ответил меньше чем на 10 ответов, то очки не начисляются`, () => {
+    assert.equal(calculateScore([], 0), -1);
+    assert.equal(calculateScore([0, 0], 1), -1);
+    assert.notEqual(calculateScore([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 1), -1);
   });
+
+  it(`Если у игрока кончились жизни, то очки не начисляются`, () => {
+    assert.equal(calculateScore([1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 0), -1);
+  });
+
+  it(`Если у игрока остались все жизни и он ответил на все вопросы не быстро и не медленно, то начисляется 1150 очков`, () => {
+    assert.equal(calculateScore([1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 3), 1150);
+  });
+
+  it(`Если игрок ответил на все вопросы быстро и сохранил все жизни, то ему наисляется 1650`, () => {
+    assert.equal(calculateScore([3, 3, 3, 3, 3, 3, 3, 3, 3, 3], 3), 1650);
+  });
+
+});
+
+describe(`Тестирование функции создания таймера`, () => {
+
+  it(`Если задано отрицательное значение таймера, он инициализируется нулём`, () => {
+    const timer = new Timer(-10);
+    assert.equal(timer.tick(), 0);
+  });
+
+  it(`Если время вышло, tick() возвращает 0`, () => {
+    const timer = new Timer(1);
+    assert.equal(timer.tick(), 0);
+  });
+
+  it(`Каждый запуск tick() уменьшает таймер на 1`, () => {
+    const timer = new Timer(10);
+    assert.equal(timer.tick(), 9);
+    assert.equal(timer.tick(), 8);
+    assert.equal(timer.tick(), 7);
+  });
+
+  it(`Если таймеру задано число больше, чем максимально допустимое, то он инициализируется нулем`, () => {
+    const timer = new Timer(Number.MAX_SAFE_INTEGER + 1);
+    assert.equal(timer.time, 0);
+  });
+
 });
