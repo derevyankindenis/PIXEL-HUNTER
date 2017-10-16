a335707da37b3eeb17b0a0336b95babcd4fedb98
Denis <derevyankindenis@mail.ru>
2017-10-17 00:12:31 +0300

    Расширил возможные ответы пользователя

--- js/data/game-data.test.js
+++ js/data/game-data.test.js
@@ -9,10 +9,30 @@ const FA = {
   time: FAST_TIME
 };
 
+const FA1 = {
+  isCorrect: true,
+  time: FAST_TIME - 1
+};
+
+const FA2 = {
+  isCorrect: true,
+  time: FAST_TIME - 2
+};
+
 // slow right answer
 const SA = {
   isCorrect: true,
   time: SLOW_TIME
+};
+
+const SA1 = {
+  isCorrect: true,
+  time: SLOW_TIME + 1
+};
+
+const SA2 = {
+  isCorrect: true,
+  time: SLOW_TIME + 2
 };
 
 // normal answer
@@ -27,13 +47,23 @@ const WA = {
   time: NORMAL_TIME
 };
 
+const WA1 = {
+  isCorrect: false,
+  time: FAST_TIME - 1
+};
+
+const WA2 = {
+  isCorrect: false,
+  time: SLOW_TIME + 2
+};
+
 describe(`Тестирование функции подсчета очков`, () => {
   it(`Если игрок ответил меньше чем на 10 ответов, то очки не начисляются`, () => {
     assert.equal(calculateScore([], 0), -1);
-    assert.equal(calculateScore([WA, WA], 1), -1);
-    assert.equal(calculateScore([WA, NA, FA, SA, WA, NA], 1), -1);
-    assert.notEqual(calculateScore([WA, WA, WA, WA, WA, WA, WA, WA, WA, WA], 1), -1);
-    assert.notEqual(calculateScore([FA, NA, WA, SA, FA, NA, WA, SA, FA, NA], 1), -1);
+    assert.equal(calculateScore([WA1, WA], 1), -1);
+    assert.equal(calculateScore([WA, NA, FA, SA2, WA1, NA], 1), -1);
+    assert.notEqual(calculateScore([WA1, WA2, WA, WA1, WA2, WA, WA1, WA, WA1, WA], 1), -1);
+    assert.notEqual(calculateScore([FA, NA, SA, SA1, FA2, NA, WA, SA2, FA, NA], 1), -1);
   });
 
   it(`Если у игрока кончились жизни, то очки не начисляются`, () => {
@@ -45,7 +75,7 @@ describe(`Тестирование функции подсчета очков`, 
   });
 
   it(`Если игрок ответил на все вопросы быстро и сохранил все жизни, то ему наисляется 1650`, () => {
-    assert.equal(calculateScore([FA, FA, FA, FA, FA, FA, FA, FA, FA, FA], 3), 1650);
+    assert.equal(calculateScore([FA, FA1, FA2, FA1, FA, FA2, FA1, FA, FA, FA], 3), 1650);
   });
 
 });
