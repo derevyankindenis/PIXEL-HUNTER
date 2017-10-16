b85a48ad3e8ec2e07feda315545e5a3f6ff1695c
Denis <derevyankindenis@mail.ru>
2017-10-16 10:05:01 +0300

    Тесты таймера вынесены в отдельный файл

--- js/data/game-data.test.js
+++ js/data/game-data.test.js
@@ -1,6 +1,5 @@
 import assert from 'assert';
 import {calculateScore} from './game-data';
-import Timer from '../timer';
 
 describe(`Тестирование функции подсчета очков`, () => {
   it(`Если игрок ответил меньше чем на 10 ответов, то очки не начисляются`, () => {
@@ -21,32 +20,6 @@ describe(`Тестирование функции подсчета очков`, 
 
   it(`Если игрок ответил на все вопросы быстро и сохранил все жизни, то ему наисляется 1650`, () => {
     assert.equal(calculateScore([150, 150, 150, 150, 150, 150, 150, 150, 150, 150], 3), 1650);
-  });
-
-});
-
-describe(`Тестирование функции создания таймера`, () => {
-
-  it(`Если задано отрицательное значение таймера, он инициализируется нулём`, () => {
-    const timer = new Timer(-10);
-    assert.equal(timer.tick(), 0);
-  });
-
-  it(`Если время вышло, tick() возвращает 0`, () => {
-    const timer = new Timer(1);
-    assert.equal(timer.tick(), 0);
-  });
-
-  it(`Каждый запуск tick() уменьшает таймер на 1`, () => {
-    const timer = new Timer(10);
-    assert.equal(timer.tick(), 9);
-    assert.equal(timer.tick(), 8);
-    assert.equal(timer.tick(), 7);
-  });
-
-  it(`Если таймеру задано число больше, чем максимально допустимое, то он инициализируется нулем`, () => {
-    const timer = new Timer(Number.MAX_SAFE_INTEGER + 1);
-    assert.equal(timer.time, 0);
   });
 
 });
