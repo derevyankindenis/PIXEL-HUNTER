8b3dfc438f401dd54750ac3309c67a4096b8a8e5
derevyankindenis <derevyankindenis@mail.ru>
2017-10-30 10:48:40 +0300

    Переназвал перечисление в соответствии с критерием Б7

--- js/screens/header/header.js
+++ js/screens/header/header.js
@@ -2,7 +2,7 @@ import HeaderView from './header-view';
 import {changeView} from '../../utils';
 import greeting from '../greeting/greeting';
 
-const BLINK_TIMER_PARAM = {
+const BlinkTimerParametrs = {
   TIME_FROM: 0,
   TIME_TO: 5,
   TIME_DELAY: 500
@@ -26,10 +26,10 @@ export default (data, state, timer, blinkTimerParam) => {
   }
 
   const blinkTimer = (time) => {
-      if ((time > BLINK_TIMER_PARAM.TIME_FROM) && (time <= BLINK_TIMER_PARAM.TIME_TO)) {
+      if ((time > BlinkTimerParametrs.TIME_FROM) && (time <= BlinkTimerParametrs.TIME_TO)) {
         setTimeout(() => {
           headerView.timer = '';
-        }, BLINK_TIMER_PARAM.TIME_DELAY);
+        }, BlinkTimerParametrs.TIME_DELAY);
     }
   }
 
