78bd66fa8d0f2383d5af7b5de35dbbc777213679
derevyankindenis <derevyankindenis@mail.ru>
2017-10-30 10:35:20 +0300

    Сделал мерцание таймера

--- js/screens/header/header-view.js
+++ js/screens/header/header-view.js
@@ -46,7 +46,6 @@ class HeaderView extends AbstractView {
 
   onLoad() {
   }
-
 }
 
 export default HeaderView;
--- js/screens/header/header.js
+++ js/screens/header/header.js
@@ -2,8 +2,13 @@ import HeaderView from './header-view';
 import {changeView} from '../../utils';
 import greeting from '../greeting/greeting';
 
+const BLINK_TIMER_PARAM = {
+  TIME_FROM: 0,
+  TIME_TO: 5,
+  TIME_DELAY: 500
+}
 
-export default (data, state, timer) => {
+export default (data, state, timer, blinkTimerParam) => {
 
   const headerView = new HeaderView(data, state);
   headerView.onClickBack = () => {
@@ -16,7 +21,16 @@ export default (data, state, timer) => {
   if (timer) {
     timer.onTick = (time) => {
       headerView.timer = time;
+      blinkTimer(time);
     };
+  }
+
+  const blinkTimer = (time) => {
+      if ((time > BLINK_TIMER_PARAM.TIME_FROM) && (time <= BLINK_TIMER_PARAM.TIME_TO)) {
+        setTimeout(() => {
+          headerView.timer = '';
+        }, BLINK_TIMER_PARAM.TIME_DELAY);
+    }
   }
 
   return headerView;
